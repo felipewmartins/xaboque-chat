f8a178ba1510e2743afaec1ad195f5c2d09955ca
felipe.martins <felipewmartins@gmail.com>
2016-06-28 21:29:46 -0300

    Using bootstrap

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var express = require('express')
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/page/index.html');
 });
+
+app.use(express.static(__dirname + '/bootstrap'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -13,7 +16,6 @@ io.on('connection', function(socket){
   });
    socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-    Push.create('Hello World!');
   });
 });
 
