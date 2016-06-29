a87a590155a1ae30de1c542ba282bc590b24200a
Felipe Martins <felipewmartins@gmail.com>
2016-06-29 14:54:31 -0300

    Adjusts on server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var express = require('express')
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/page/index.html');
 });
@@ -19,6 +21,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(app.get('port'), function(){
+  console.log('listening on *'+app.get('port'));
 });
